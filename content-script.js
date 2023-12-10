74760af2867369fc11a1f27591ab84bd363c032f
Alan Sorani <al.sorani@proton.me>
2023-12-10 17:41:10 +0100

    fixed script working only on dictionary entries

--- content-script.js
+++ content-script.js
@@ -1,22 +1,32 @@
-// add the css code for styling the anki buttons
-console.log("content script sending message");
-browser.runtime.sendMessage("add CSS");
+function matchURL(url) {
+  regex = new RegExp("^https:\/\/.+\.wiktionary\.org\/[^:]+$")
+  return regex.test(url)
+}
 
-//go over the language headers to add buttons next to each of them
-const language_headers = document.getElementsByTagName("h2");
+// Get the URL of the active tab
+url = window.location.href;
+if (matchURL(url)) {
 
-for (header of language_headers){
-	if (header.innerHTML != "Contents" && header.innerHTML != "Navigation menu"){
-		//create button for adding data to Anki
-		const button_span = document.createElement("span");
-		button_span.setAttribute("class", "mw-ankibutton");
-		button_span.setAttribute("style", "padding-left:17px; display:inline-block;");
-		const button = document.createElement("button");
-		const button_text = document.createTextNode("Add to Anki");
+  // Add the css code for styling the anki buttons
+  console.log("content script sending message");
+  browser.runtime.sendMessage("add CSS");
 
-		//append the button to the DOM node
-		button_span.appendChild(button);
-		button.appendChild(button_text);
-		header.appendChild(button_span);
-	}
+  // Go over the language headers to add buttons next to each of them
+  const language_headers = document.getElementsByTagName("h2");
+
+  for (header of language_headers){
+    if (header.innerHTML != "Contents" && header.innerHTML != "Navigation menu"){
+      //create button for adding data to Anki
+      const button_span = document.createElement("span");
+      button_span.setAttribute("class", "mw-ankibutton");
+      button_span.setAttribute("style", "padding-left:17px; display:inline-block;");
+      const button = document.createElement("button");
+      const button_text = document.createTextNode("Add to Anki");
+
+      //append the button to the DOM node
+      button_span.appendChild(button);
+      button.appendChild(button_text);
+      header.appendChild(button_span);
+    }
+  }
 }
