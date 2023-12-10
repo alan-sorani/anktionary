5a47d8c3d728a94e872fc1542a922827dd3d92dc
Alan Sorani <al.sorani@proton.me>
2023-12-10 16:44:11 +0100

    moved adding CSS to background file (requires fixing)

--- background-script.js
+++ background-script.js
@@ -1,23 +1,22 @@
 /*
 Log that we received the message.
-Then display a notification. The notification contains the URL,
-which we read from the message.
+Then add the CSS for Anki buttons to the CSS of the active tab
 */
-function add_css(message) {
+function addCSS(message) {
 
-  console.log("background script received message");
+	console.log("background script received message");
 
-  // Create an error in case insertCSS fails
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-  
-  // Insert CSS for custom buttons
-  let insertingCSS = browser.tabs.insertCSS({file: "anki-buttons.css"});
-  insertingCSS.then(null,onError);
+	// Create an error in case insertCSS fails
+	function onError(error) {
+		console.log(`Error: ${error}`);
+	}
+
+	// Insert CSS for custom buttons
+	let insertingCSS = browser.tabs.insertCSS({file: "anki-buttons.css"});
+	insertingCSS.then(null,onError);
 }
 
 /*
-Assign `notify()` as a listener to messages from the content script.
+Assign `addCSS()` as a listener to messages from the content script.
 */
-browser.runtime.onMessage.addListener(add_css);
+browser.runtime.onMessage.addListener(addCSS);
--- content-script.js
+++ content-script.js
@@ -3,20 +3,20 @@ console.log("content script sending message");
 browser.runtime.sendMessage("add CSS");
 
 //go over the language headers to add buttons next to each of them
-const language_headers = document.getElementsByTagName("h2")
+const language_headers = document.getElementsByTagName("h2");
 
 for (header of language_headers){
 	if (header.innerHTML != "Contents" && header.innerHTML != "Navigation menu"){
 		//create button for adding data to Anki
-		const button_span = document.createElement("span")
-		button_span.setAttribute("class", "mw-ankibutton")
-		button_span.setAttribute("style", "padding-left:17px; display:inline-block;")
-		const button = document.createElement("button")
-		const button_text = document.createTextNode("Add to Anki")
+		const button_span = document.createElement("span");
+		button_span.setAttribute("class", "mw-ankibutton");
+		button_span.setAttribute("style", "padding-left:17px; display:inline-block;");
+		const button = document.createElement("button");
+		const button_text = document.createTextNode("Add to Anki");
 
 		//append the button to the DOM node
-		button_span.appendChild(button)
-		button.appendChild(button_text)
-		header.appendChild(button_span)
+		button_span.appendChild(button);
+		button.appendChild(button_text);
+		header.appendChild(button_span);
 	}
 }
